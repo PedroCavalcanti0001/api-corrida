9421547ebe13f1f5c529daff9e778b3c25594512
pedro <pedroeugeniocavalcanti@gmail.com>
2022-09-29 09:51:03 -0300

    Propriedade lapTime do RunningLogDto agora é mostrada em segundos

--- src/main/java/me/pedroeugenio/apicorrida/presenter/dto/RunningLogDto.java
+++ src/main/java/me/pedroeugenio/apicorrida/presenter/dto/RunningLogDto.java
@@ -1,5 +1,6 @@
 package me.pedroeugenio.apicorrida.presenter.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +18,11 @@ public class RunningLogDto {
     private Integer lapNumber;
     private Duration lapTime;
     private Float averageSpeedBack;
+
+
+    @JsonProperty("lapTime")
+    public long getLapTime() {
+        return lapTime.getSeconds();
+    }
 
 }
